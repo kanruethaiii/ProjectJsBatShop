a919166ba821e6489ab6ad2b7b4435ad1710a1ee
gun <99672136+kanruethaiii@users.noreply.github.com>
2024-03-04 02:47:41 +0700

    d

--- Front_end/server_front.js
+++ Front_end/server_front.js
@@ -6,7 +6,7 @@ const { env } = require('process');
 const app = express();
 
 //const base_url = "http://localhost:3000"
-const base_url = "node59722-jsbatshop.proen.app.ruk-com.cloud"
+const base_url = "http://node59719-jsbatshop.proen.app.ruk-com.cloud"
 
 app.set("views" , path.join(__dirname , "/public/views"))
 app.set("view engine" , "ejs")
